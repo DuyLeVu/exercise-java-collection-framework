2a37afb098aebf2a013a2ccbe67aa855ea7c1b7a
DuyLeVu <levuduy132@gmail.com>
2021-10-22 16:23:56 +0700

    Add hashmap-linkedlist - treemap

---
+++ dictionary/src/com/company/Dictionary.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dictionary {
+    HashMap<String, Integer> dictionary = new HashMap<>();
+
+
+    }
+// hashMap.put("Smith", 30);
+// hashMap.put("Anderson", 31);
+// hashMap.put("Lewis", 29);
+// hashMap.put("Cook", 29);
+// System.out.println("Display entries in HashMap");
+// System.out.println(hashMap + "\n");
+}
